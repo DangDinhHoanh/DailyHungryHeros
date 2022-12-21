a2b483e7628a549710f38bab293e47bbbd89acb3
ashTT3008 <0306201446@caothang.edu.vn>
2022-12-21 14:47:03 +0700

    Update-Single_screen

--- lib/screens/single_screen.dart
+++ lib/screens/single_screen.dart
@@ -209,4 +209,100 @@ class _SingleScreenState extends State<SingleScreen> {
       ),
     );
   }
+
+  Container buildQuestion(Question question) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width - 20,
+            height: MediaQuery.of(context).size.height / 2.5,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 222, 210, 97),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  question.question,
+                  textScaleFactor: 1.2,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width - 20,
+            child: Answer(
+              question: question,
+              lock: lock,
+              select: select,
+              onClick: ((value) {
+                setState(() {
+                  lock = true;
+                  select.add(value);
+                });
+                if (value == question.correct) {
+                  score++;
+                  setState(() {
+                    lock = false;
+                    select.clear();
+                  });
+                  if (questionIndex < widget.lsQuestions.length) {
+                    _controller.nextPage(
+                        duration: const Duration(milliseconds: 250),
+                        curve: Curves.bounceInOut);
+                    setState(() {
+                      questionIndex++;
+                    });
+                  } else {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Result(
+                          heart: heart,
+                          level: widget.level,
+                          result: score,
+                          time: time,
+                        ),
+                      ),
+                    );
+                  }
+                } else {
+                  setState(() {
+                    if (heart != 0) {
+                      heart--;
+                    } else {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Result(
+                            heart: heart,
+                            level: widget.level,
+                            result: score,
+                            time: time,
+                          ),
+                        ),
+                      );
+                    }
+                  });
+                }
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
